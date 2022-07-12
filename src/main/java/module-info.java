3a5562ab515ba54b9d0c24405e5bef2a2be22d4f
Hassen Younsi <74210696+7asyou@users.noreply.github.com>
2022-07-12 10:12:32 +0100

    working controllers folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module techaholic.recruited {
+	requires transitive javafx.graphics;
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires fr.brouillard.oss.cssfx;
 
 	opens techaholic.recruited to javafx.fxml;
+	opens techaholic.recruited.controllers to javafx.fxml;
 
 	exports techaholic.recruited;
 }
